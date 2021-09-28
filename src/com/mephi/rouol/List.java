65e9738bd80d47c2fbc9b14c97c5d1b35e69a914
rouol <karrauln@yandex.ru>
2021-09-28 20:21:23 +0300

    finished List class

---
+++ src/com/mephi/rouol/List.java
@@ -0,0 +1,134 @@
+package com.mephi.rouol;
+
+//import java.util.Arrays;
+
+public class List {
+
+    // Defaults
+    private static final int DEFAULT_BUFFER_SIZE = 10;
+    //private static final int DEFAULT_CAPACITY = DEFAULT_BUFFER_SIZE * 3;
+
+    // count
+    private int count;
+    //private int LCap = DEFAULT_BUFFER_SIZE;
+    private int RCap = DEFAULT_BUFFER_SIZE;
+
+    //This array will store all elements added to list
+    private Object[] data;
+
+    //                        CONSTRUCTOR
+    //-----------------------------------------------------------
+    // empty
+    public List() {
+        data = new Object[DEFAULT_BUFFER_SIZE];
+        count = 0;
+    }
+    // by size and value
+    public List(int size, Object defaultValue) {
+        data = new Object[size + DEFAULT_BUFFER_SIZE];
+        for (int i = 0; i < size; i++){
+            data[i] = defaultValue;
+        }
+        count = size;
+    }
+
+    //                          METHODS
+    //-----------------------------------------------------------
+    /** add object to the end **/
+    public void add(Object object) {
+
+        if (this.RCap > 0) {
+            data[count++] = object;
+            RCap--;
+        } else {
+            refreshRCap();
+            data[count++] = object;
+            RCap--;
+        }
+    }
+    /** set object by index **/
+    public void add(Object object, int index) {
+        if (index > this.count || index < 0) {   // add to this.count = add to the end
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + count);
+        }
+        if (this.count == this.data.length) refreshRCap();
+        System.arraycopy(data, index, this.data, index + 1, this.count - index);
+        this.count++;
+        data[index] = object;
+    }
+    // returns object by index
+    public Object get(int index) {
+        if (index >= count || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + count);
+        }
+        return data[index];
+    }
+    // sets null and returns old object by index
+    public Object remove(int index) {
+        if (index >= count || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + count);
+        }
+        Object _object = this.data[index];
+        if (this.count - (index + 1) >= 0)
+            System.arraycopy(data, index + 1, data, index + 1 - 1, this.count - (index + 1));
+        data[this.count - 1] = null;
+        this.count--;
+        return _object;
+    }
+    // sets new object and returns old object
+    public Object set(Object object, int index) {
+        if (index >= count || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + count);
+        }
+        Object _object = data[index];
+        data[index] = object;
+        return _object;
+    }
+    // returns true if list contains object, false if not
+    public Boolean contains(Object object) {
+        for (int i = 0; i < this.count; i++) {
+            if (this.get(i) == object) return true;
+        }
+        return false;
+    }
+    // returns index of object
+    public int indexOf(Object object) {
+        for (int i = 0; i < this.count; i++) {
+            if (this.get(i) == object) return i;
+        }
+        return -1;
+    }
+    // returns current size of list
+    public int size() {
+        return this.count;
+    }
+    // returns true if list is empty, false if not
+    public Boolean isEmpty() {
+        return this.count == 0;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for(int i = 0; i < this.count; i++) {
+            sb.append(this.get(i).toString());
+            if (i < this.count - 1){
+                sb.append(",");
+            }
+        }
+        sb.append(']');
+        return sb.toString();
+    }
+
+    //                     PRIVATE FUNCTIONS
+    //-----------------------------------------------------------
+    private void refreshRCap() {
+        //data = Arrays.copyOf(data, this.data.length + DEFAULT_BUFFER_SIZE);
+        Object[] _data = this.data;
+        this.data = new Object[this.data.length + DEFAULT_BUFFER_SIZE];
+        System.arraycopy(_data, 0, this.data, 0, this.count);
+    }
+
+}
--- src/com/mephi/rouol/Main.java
+++ src/com/mephi/rouol/Main.java
@@ -3,14 +3,38 @@ package com.mephi.rouol;
 public class Main {
 
     public static void main(String[] args) {
-        DynamicArray<Integer> dynamicArray = new DynamicArray<Integer>(10, 0);
+        List list = new List();
 
-        //Add elements
-        dynamicArray.add(1);
-        dynamicArray.add(2);
-        dynamicArray.add(3);
-        System.out.println(dynamicArray.get(10));
-        System.out.println(dynamicArray.size());
-        System.out.println(dynamicArray);
+        System.out.println(list.isEmpty());
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println(list.get(0));
+
+        System.out.println(list.contains(8));
+        list.add(8, 1);
+        System.out.println(list.contains(8));
+        System.out.println(list.indexOf(8));
+
+        System.out.println(list.size());
+
+        System.out.println(list.isEmpty());
+
+        System.out.println(list);
+        System.out.println(list.remove(1));
+        System.out.println(list);
+
+        list.add(10, 4);
+        System.out.println(list);
+
+        System.out.println(list.set(100, 4));
+        System.out.println(list);
+
+        List newlist = new List(10, "sdjfhjshdfkjsh");
+        System.out.println(newlist);
     }
 }
