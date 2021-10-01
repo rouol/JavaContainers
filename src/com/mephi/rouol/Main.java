972b49479b27b0f537e684e415c032c2c0b53754
rouol <karrauln@yandex.ru>
2021-10-01 20:32:43 +0300

    added new function mergeSortedLists

--- src/com/mephi/rouol/Main.java
+++ src/com/mephi/rouol/Main.java
@@ -2,6 +2,43 @@ package com.mephi.rouol;
 
 public class Main {
 
+    /** returns merged sorted list of two given sorted lists **/
+    public static List mergeSortedLists(List A, List B){
+
+        if (A.size() == 0) return B;
+        if (B.size() == 0) return A;
+
+        int newlen = A.size() + B.size();
+        List mergedList = new List(newlen);
+        int headA = 0;
+        int headB = 0;
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < newlen; i++) {
+            if (headA >= A.size()) {
+                mergedList.set(b, i);
+                headB++;
+                continue;
+            }
+            if (headB >= B.size()) {
+                mergedList.set(a, i);
+                headA++;
+                continue;
+            }
+            a = (int) A.get(headA);
+            b = (int) B.get(headB);
+            if (a <= b) {
+                mergedList.set(a, i);
+                headA++;
+            } else {
+                mergedList.set(b, i);
+                headB++;
+            }
+        }
+
+        return mergedList;
+    }
+
     public static void main(String[] args) {
 
         // checking the List
@@ -43,31 +80,41 @@ public class Main {
         }
 
         // checking the Map
-        System.out.println("\n        checking the Map\n");
+        {
+            System.out.println("\n        checking the Map\n");
 
-        Map map = new Map();
+            Map map = new Map();
 
-        System.out.println(map.isEmpty());
+            System.out.println(map.isEmpty());
 
-        map.put(0, 10);
-        map.put(1, "ааа");
-        map.put(17, new List(3, "ыыы"));
-        map.put(2, 228);
-        map.put(3, "java");
-        map.put("dsjfkl", "sdfsdfsdf");
-        System.out.println(map);
-        System.out.println(map.get("dsjfkl"));
-        System.out.println(map.remove(1));
-        System.out.println(map.keyContains(100));
-        System.out.println(map.get(100,"100"));
-        System.out.println(map.keyContains(100));
-        System.out.println(map);
-        System.out.println(map.getKeys().toString());
-        System.out.println(map.getValues().toString());
-        System.out.println(map.getEntries().toString());
-        System.out.println(map.size());
+            map.put(0, 10);
+            map.put(1, "ааа");
+            map.put(17, new List(3, "ыыы"));
+            map.put(2, 228);
+            map.put(3, "java");
+            map.put("dsjfkl", "sdfsdfsdf");
+            System.out.println(map);
+            System.out.println(map.get("dsjfkl"));
+            System.out.println(map.remove(1));
+            System.out.println(map.keyContains(100));
+            System.out.println(map.get(100, "100"));
+            System.out.println(map.keyContains(100));
+            System.out.println(map);
+            System.out.println(map.getKeys().toString());
+            System.out.println(map.getValues().toString());
+            System.out.println(map.getEntries().toString());
+            System.out.println(map.size());
 
-        System.out.println(map.isEmpty());
+            System.out.println(map.isEmpty());
+        }
+
+        // checking mergeSortedLists
+        {
+            List a = new List(new Object[]{0, 1, 2, 7, 9});
+            List b = new List(new Object[]{-1, 2, 3, 4, 5, 8, 11});
+            List merged = mergeSortedLists(a, b);
+            System.out.println(merged);
+        }
 
     }
 }
