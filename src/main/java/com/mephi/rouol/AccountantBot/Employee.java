32778a426b1979d392b7edcea0fbb22b9d68e3d1
rouol <karrauln@yandex.ru>
2021-11-16 18:13:20 +0300

    moved EmployeeBuilder inside Employee class

--- src/main/java/com/mephi/rouol/AccountantBot/Employee.java
+++ src/main/java/com/mephi/rouol/AccountantBot/Employee.java
@@ -83,6 +83,73 @@ public class Employee {
         this.code = code;
     }
 
+    private static class EmployeeBuilder {
+        private String givenName;
+        private String surName;
+        private int age;
+        private Employee.Gender gender;
+        private Employee.Role role;
+        private Employee.Dept dept;
+        private String eMail;
+        private String phone;
+        private String address;
+        private String city;
+        private String state;
+        private int code;
+
+        public void setGivenName(String givenName) {
+            this.givenName = givenName;
+        }
+
+        public void setSurName(String surName) {
+            this.surName = surName;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public void setGender(Gender gender) {
+            this.gender = gender;
+        }
+
+        public void setRole(Role role) {
+            this.role = role;
+        }
+
+        public void setDept(Dept dept) {
+            this.dept = dept;
+        }
+
+        public void seteMail(String eMail) {
+            this.eMail = eMail;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public Employee createEmployee() {
+            return new Employee(givenName, surName, age, gender, role, dept, eMail, phone, address, city, state, code);
+        }
+    }
+
     // getters/setters
 
     public String getGivenName() {
